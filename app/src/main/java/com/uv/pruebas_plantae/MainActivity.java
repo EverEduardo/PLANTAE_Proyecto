1b3a860641e160fa48dbcc511f0bc2f164bd2fdb
Ever Eduardo <evergaminouv@gmail.com>
2023-04-07 22:16:09 -0600

    Se agrego funcionalidad Registro

--- app/src/main/java/com/uv/pruebas_plantae/MainActivity.java
+++ app/src/main/java/com/uv/pruebas_plantae/MainActivity.java
@@ -5,13 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText user, pass;
+    Button btnEntrar, btnIrRegistro;
+    dbUsuario dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (EditText) findViewById(R.id.usuario);
+        pass = (EditText) findViewById(R.id.contrasena);
+        btnEntrar = (Button) findViewById(R.id.btn_IniciarSesion);
+        btnIrRegistro = (Button) findViewById(R.id.btn_irRegistro);
+
+        dao = new dbUsuario(this);
+
+    }
+
+    public void iniciarSeson(View view){
+        String u= user.getText().toString();
+        String c= pass.getText().toString();
+        if(u.equals("")||c.equals("")){
+            Toast.makeText(this, "Error: Campos vacios", Toast.LENGTH_LONG).show();
+        } else if (dao.login(u,c)==1) {
+            Toast.makeText(this, "Datos correctos", Toast.LENGTH_LONG).show();
+            Intent i3 = new Intent(this, Barra_navegacion.class);
+            startActivity(i3);
+        }else{
+            Toast.makeText(this, "Usuario y/o contraseña incorrecta", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void irMenu(View view){
@@ -23,4 +52,8 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, Registro.class);
         startActivity(i);
     }
+
+
+
+
 }
